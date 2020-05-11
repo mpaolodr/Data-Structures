3885fe7e7b3adb10ebcc00483e00398dd709306f
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-11 15:55:15 -0400

    implemented queue with array and linkedlist as storage structure

--- queue/queue.py
+++ queue/queue.py
@@ -15,66 +15,107 @@ Stretch: What if you could only use instances of your Stack class to implement t
 """
 
 
-class Node:
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next = next_node
+# class Node:
+#     def __init__(self, data, next_node=None):
+#         self.data = data
+#         self.next = next_node
 
-    def get_data(self):
-        return self.data
+#     def get_data(self):
+#         return self.data
 
-    def get_next(self):
-        return self.next
+#     def get_next(self):
+#         return self.next
 
-    def set_next(self, new_next):
-        self.next = new_next
-
-
-class SinglyLinkedList:
-    def __init__(self, node=None):
-        self.head = None
-        self.tail = None
-
-    def add_to_head(self, data):
-        new_node = Node(data)
-
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-
-    def add_to_tail(self, data):
-        new_node = Node(data)
-
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-
-    def remove_from_head(self):
-        if not self.head and not self.tail:
-            return
-        else:
-
-            value = self.head.data
-            self.head = self.head.next
-            return value
+#     def set_next(self, new_next):
+#         self.next = new_next
 
 
+# class SinglyLinkedList:
+#     def __init__(self, node=None):
+#         self.head = None
+#         self.tail = None
+
+#     def add_to_head(self, data):
+#         new_node = Node(data)
+
+#         if not self.head:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             new_node.next = self.head
+#             self.head = new_node
+
+#     def add_to_tail(self, data):
+#         new_node = Node(data)
+
+#         if not self.head:
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             if self.head == self.tail:
+#                 self.head.next = new_node
+#                 self.tail = new_node
+#             else:
+#                 self.tail.next = new_node
+#                 self.tail = new_node
+
+#     def remove_from_head(self):
+
+#         if not self.head and not self.tail:
+#             return
+#         else:
+#             data = self.head.data
+
+#             if self.head == self.tail:
+#                 self.head = None
+#                 self.tail = None
+#                 return data
+#             else:
+#                 data = self.head.data
+#                 self.head = self.head.next
+#                 return data
+
+
+# # using Linked list as storage structure
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = SinglyLinkedList()
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+
+#     def dequeue(self):
+#         if self.size == 0:
+#             return
+#         else:
+#             self.size -= 1
+#             return self.storage.remove_from_head()
+
+
+# using array as storage structure
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return
+        else:
+            self.size -= 1
+            data = self.storage[0]
+            self.storage.pop(0)
+
+            return data
