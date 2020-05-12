3738458c6d70dae2d1ea40330638eae8df04da73
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-12 16:00:53 -0400

    remove_from_head test passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -75,7 +75,23 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        pass
+        if not self.head and not self.tail:
+            return
+
+        self.length -= 1
+        if self.head == self.tail:
+            value = self.head.value
+            self.head = None
+            self.tail = None
+
+            return value
+
+        else:
+            value = self.head.value
+            self.head = self.head.next
+            self.head.prev = None
+
+            return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
