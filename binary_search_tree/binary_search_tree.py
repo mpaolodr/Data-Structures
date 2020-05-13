725df38fbcefcf74510931bed4f670c0c68177fd
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-13 17:02:26 -0400

    part 2 complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
-the data they store. That ordering in turn makes it a lot more efficient 
-at searching for a particular piece of data in the tree. 
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
@@ -102,25 +102,76 @@ class BinarySearchTree:
     # Hint:  Use a recursive, depth first traversal
 
     def in_order_print(self, node):
-        pass
+        if node:
+            self.in_order_print(node.left)
+
+            print(node.value)
+
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
+    def bft_print(self, node):
+        h = self.height(node)
+
+        for i in range(1, h+1):
+            self.printGivenLevel(node, i)
+
+            # Print the value of every node, starting with the given node,
+            # in an iterative depth first traversal
+
     def dft_print(self, node):
-        pass
+        if node:
+
+            print(node.value)
+
+            self.dft_print(node.left)
+
+            self.dft_print(node.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        if node:
+            print(node.value)
+
+            self.pre_order_dft(node.left)
+
+            self.pre_order_dft(node.right)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node:
+
+            self.post_order_dft(node.left)
+
+            self.post_order_dft(node.right)
+            print(node.value)
+
+    def height(self, node):
+        if node is None:
+            return 0
+
+        else:
+            left_height = self.height(node.left)
+            right_height = self.height(node.right)
+
+            if left_height > right_height:
+                return left_height + 1
+
+            else:
+                return right_height + 1
+
+    def printGivenLevel(self, node, level):
+        if node is None:
+            return
+
+        if level == 1:
+            print(node.value)
+
+        elif level > 1:
+            self.printGivenLevel(node.left, level-1)
+            self.printGivenLevel(node.right, level-1)
